faaaa991d617c271f92e26a5d5c1607b62832962
Aximilli1212 <aximill1212@gmail.com>
2020-06-04 22:28:35 +0000

    define structure of rental

---
+++ .history/app/models/rental_20200604172804.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+const { Model } = DS;
+
+export default Model.extend({
+    title: DS.attr(),
+    owner: DS.attr(),
+    city: DS.attr(),
+    category: DS.attr(),
+    image: DS.attr(),
+    bedrooms: DS.attr(),
+    description: DS.attr()
+});

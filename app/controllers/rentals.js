5defa7f99077a4c444d0b6f2277f8d489480abc7
Aximilli1212 <aximill1212@gmail.com>
2020-06-05 15:45:29 +0000

    add readjust

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -4,11 +4,18 @@ export default Controller.extend({
   actions: {
     filterByCity(param) {
       if (param !== '') {
-        return this.store.query('rental', { city: param });
+        return this.store
+          .query('rental', { city: param }).then((results) => {
+            return { query: param, results: results };
+          });
       }
       else {
-        return this.store.findAll('rental');
+        return this.store
+          .findAll('rental').then((results) => {
+            return { query: param, results: results };
+          });
       }
     }
   }
-});
+}
+);
